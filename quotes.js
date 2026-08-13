cc488e1d36fb3a0a5443a31e3b435c5a8d438405
kakira943 <kakira943@gmail.com>
2026-08-13 17:19:20 +0900

    add quotes

---
+++ quotes.js
@@ -0,0 +1,31 @@
+const quotes = [
+    {
+        en: "Losing is not an option.",
+        ja: "負けるという選択肢はない。",
+        author: "Yamamoto Yosinobu",
+    },
+    {
+        en: "From this moment on, I’m going to ruin you. You’re capable of nothing.",
+        ja: "今からお前らを台無しにする。お前らは何もできない。",
+        author: "Yamamoto Yosinobu",
+    },
+
+    {
+        en: "It doesn’t matter. I’ll just take care of it as usual.",
+        ja: "何ら問題ない。いつも通り始末するだけだ。",
+        author: "Yamamoto Yosinobu",
+    },
+
+    {
+        en: "Losing is not an option.",
+        ja: "負けるという選択肢はない。",
+        author: "Yamamoto Yosinobu",
+    },
+    {
+        en: "I have no idea what I'm doing.",
+        jp: "自分が何をしているのか全く分からない。",
+        author: "きむらあきら",
+    },
+
+
+];
